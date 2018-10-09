f9a0b4cb626605bc3f3bd079a5bad49e4e73028b
alnoki <43892045+alnoki@users.noreply.github.com>
2018-10-09 16:47:34 -0500

    Completes up through middle of Python Beginner Tutorial 4 - intro syntax and types

---
+++ integers_and_floats.py
@@ -0,0 +1,14 @@
+# Arithmetic Operators
+# Addition:         3 + 2
+# Subtraction:      3 - 2
+# Multiplication:   3 * 2
+# Division:         3 / 2
+# Floor Division:   3 // 2
+# Exponent:         3 ** 2
+# Modulus:          3 % 2
+# Extra: *=, abs(), round(-3.75, digits_after_decimal), int(num_to_cast)
+
+num_1 = int('100')
+num_2 = int('200')
+
+print(int(num_1) + (num_2))
---
+++ intro.py
@@ -0,0 +1,9 @@
+greeting = 'Hello'
+name = 'Michael'
+
+message = '{}, {}. Welcome!'.format(greeting, name)
+message = f'{greeting.upper()}, {name}. Welcome!'
+
+print(dir(name))
+
+# Extra: type(variable), dir(variable)
